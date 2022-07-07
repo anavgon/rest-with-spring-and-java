e932df97da8dc4ef81147c38312170c4283f2453
carolina <anacarolinavgoncalves@gmail.com>
2022-07-07 12:14:50 -0300

    First steps in Java with Spring Boot 3

--- src/main/java/br/com/estudo/Startup.java
+++ src/main/java/br/com/estudo/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestWithSpringAndJavaApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestWithSpringAndJavaApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
